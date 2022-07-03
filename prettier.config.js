6fc90d25f34541938c9efbac9a9630302f093844
Eber Alejo <eber4202@hotmail.com>
2022-07-03 17:02:08 -0500

    [ADD] prettier eslint config

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  printWidth: 200,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  bracketSameLine: true,
+}
