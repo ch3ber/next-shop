ca28fef6df2e57c9130bb439aa6acae5103c92b6
Eber Alejo <eber4202@hotmail.com>
2022-07-06 21:06:59 -0500

    :sparkles: [FEAT] global google analytics
    Add a script in app to set gobal analytic in the app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Script from 'next/script'
 
 import { AppContext } from '@context/AppContext'
 import { useInitialState } from '@hooks/useInitialState'
@@ -17,6 +18,22 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Next Shop</title>
       </Head>
+      <Script
+        async
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-X233100GTE"></Script>
+      <Script
+        id="google-analytics"
+        dangerouslySetInnerHTML={{
+          html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-X233100GTE');
+        `,
+        }}
+      />
       <Header />
       <Component {...pageProps} />
     </AppContext.Provider>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,26 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <Script
-        async
-        strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-X233100GTE"></Script>
-      <Script
-        id="google-analytics"
-        dangerouslySetInnerHTML={{
-          html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-X233100GTE');
-        `,
-        }}
-      />
 
       <body>
         <Main />
