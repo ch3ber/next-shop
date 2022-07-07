d6e587ce15535f974bb64471dc81391cfd6f4dd0
Eber Alejo <eber4202@hotmail.com>
2022-07-06 20:01:50 -0500

    :bug: Global title
    Add a global title in _app.js page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,12 @@
+import Head from 'next/head'
+
 import { AppContext } from '@context/AppContext'
 import { useInitialState } from '@hooks/useInitialState'
+
+// components
 import { Header } from '@components/Header'
 
+// assets
 import '@styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
@@ -9,6 +14,9 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <AppContext.Provider value={initialState}>
+      <Head>
+        <title>Next Shop</title>
+      </Head>
       <Header />
       <Component {...pageProps} />
     </AppContext.Provider>
